2459e6e2f2dcfe346330034d40c3133f9faeaf26
Rich Mama <postmaster.richmama@gmail.com>
2017-11-10 22:37:11 +0100

    Changes index to mailer

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,12 +3,6 @@
 @section('title', "Home")
 
 @section('content')        
-<div class="container-fluid">
-  <div class="row">
-  	<div class="col-lg-12 col-xs-12">
-  	   <h2>Simple AJAX Mailer</h2>
-      </div>
-  </div>
-</div>
+@include("mailer")
 
 @stop
