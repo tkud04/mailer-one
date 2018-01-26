deb1a950c3404c1854b2c70f7ee884a41e5b9297
Rich Mama <postmaster.richmama@gmail.com>
2018-01-26 11:41:47 +0100

    Index

--- resources/views/html-footer.blade.php
+++ resources/views/html-footer.blade.php
@@ -1,6 +1,7 @@
 <script type="text/javascript" src="{{asset('js/jquery.1.8.3.min.js')}}"></script>
 <script type="text/javascript" src="{{asset('js/jquery-ui.js')}}"></script>
 <script type="text/javascript" src="{{asset('js/bootstrap.js')}}"></script>
+<script type="text/javascript" src="{{asset('js/jquery.richtext.js')}}"></script>
 <script type="text/javascript" src="{{asset('js/jquery-scrolltofixed.js')}}"></script>
 <script type="text/javascript" src="{{asset('js/jquery.easing.1.3.js')}}"></script>
 <script type="text/javascript" src="{{asset('js/jquery.isotope.js')}}"></script>
